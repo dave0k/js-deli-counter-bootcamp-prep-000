64557927405ca444e60fc4b34d72b3fce7c27d01
Dave K <dkorff12@gmail.com>
2019-01-29 00:12:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ` + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return 'Currently serving ' + katzDeliLine.shift() + '.'
+  }
+}
+
+function currentLine(line) {
+  var newLine = []
+  if (line.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    for (let i )
+  }
+}
