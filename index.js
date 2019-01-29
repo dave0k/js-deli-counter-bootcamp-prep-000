8ddea2224c3b1a13ae547eb1c73883a537afd05b
Dave K <dkorff12@gmail.com>
2019-01-29 00:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return 'The line is currently empty.'
   } else {
-    for (let i = 0, i < line.length; i++) {
+    for (let i = 0; i < line.length; i++) {
       newLine += (i + 1) + '. ' + line[i] + ', '
     }
     newLine = newLine.slice(0, newLine.length - 2)
